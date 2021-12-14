d248ba3215146258d08a4ca3f28b47fe144506fa
aman.rv <aman.rv@flipkart.com>
2021-12-14 21:24:48 +0530

    changes

--- Python Script/read_part_file.py
+++ Python Script/read_part_file.py
@@ -1,8 +1,8 @@
 count = 0
 for i in range(162):
     file = "0"*(5-len(str(i)))+str(i)
-    print("/tmp/fdp_payload/Oct2021CBCScores/*/part-{}".format(file))
-    rdd = spark.sparkContext.textFile("/tmp/fdp_payload/Oct2021CBCScores/*/part-{}".format(file))
+    print("path/*/part-{}".format(file))
+    rdd = spark.sparkContext.textFile("path/part-{}".format(file))
     temp = rdd.count()
     print(temp)
     count += temp
