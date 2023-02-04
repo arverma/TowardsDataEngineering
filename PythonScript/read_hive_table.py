9e0cb54bc4604826154a3afce2c0e74f60d0c06d
aman.rv <aman.rv@flipkart.com>
2023-02-04 23:51:38 +0530

    add un commited files

--- PythonScript/read_hive_table.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import HiveContext
-hive_context = HiveContext(sc)
-df2=hive_context.table("database_name.table_name1")
-df2=df2.drop(*["list_of_columns"])
-df3=hive_context.table("database_name.table_name2")
-df3=df3.drop(*["list_of_columns"])
-df=df2.join(df3,None,"outer")
--- pyspark/encrypt_decryt.py
+++ pyspark/encrypt_decryt.py
@@ -1,7 +1,7 @@
 from pyspark.sql.functions import lit, udf
 from cryptography.fernet import Fernet
 from pyspark.sql.types import StringType
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Row
 
 
 def encrypt_val(clear_text, MASTER_KEY):
@@ -21,13 +21,31 @@ def decrypt_val(cipher_text, MASTER_KEY):
 if __name__ == '__main__':
     spark = SparkSession.builder.appName("Test Job").getOrCreate()
 
-    df = spark.read.csv("sample_file.csv", header=True)
+    #     df = spark.read.csv("sample_file.csv", header=True)
+    # create a list of rows with the data
+    data = [
+        Row("John Doe", "IT", "201901", 50000),
+        Row("Jane Doe", "HR", "202001", 60000),
+        Row("Bob Smith", "IT", "201905", 70000),
+        Row("Alice Smith", "HR", "202011", 80000),
+        Row("James Johnson", "IT", "202002", 90000),
+        Row("Emily Johnson", "HR", "201310", 100000),
+        Row("David Williams", "IT", "201511", 110000),
+        Row("Samantha Williams", "HR", "202207", 120000),
+        Row("Charles Brown", "IT", "202101", 130000),
+        Row("Ashley Brown", "HR", "202111", 140000)
+    ]
+
+    # create the DataFrame
+    df = spark.createDataFrame(data, schema)
 
     encrypt = udf(encrypt_val, StringType())
     decrypt = udf(decrypt_val, StringType())
 
     encryptionKey = Fernet.generate_key()
 
-    df = df.withColumn("first_name_encrypted", encrypt(df.first_name, lit(encryptionKey)))
-    df = df.withColumn("first_name_decrypted", decrypt(df.first_name_encrypted, lit(encryptionKey)))
-    df.coalesce(1).write.mode("overwrite").csv("output", header=True)
+    df = df.withColumn("first_name_encrypted", encrypt(df.employee_name, lit(encryptionKey)))
+    df = df.withColumn("first_name_decrypted", decrypt(df.employee_name, lit(encryptionKey)))
+
+    df.show()
+#     df.coalesce(1).write.mode("overwrite").csv("output", header=True)
